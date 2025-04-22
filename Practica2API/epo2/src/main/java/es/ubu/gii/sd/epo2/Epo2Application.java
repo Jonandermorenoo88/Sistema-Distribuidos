b99f622a750edde1edd888ded9bedb0138886bdb
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-04-22 10:49:16 +0200

    Subido el spring initializr

---
+++ Practica2API/epo2/src/main/java/es/ubu/gii/sd/epo2/Epo2Application.java
@@ -0,0 +1,13 @@
+package es.ubu.gii.sd.epo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epo2Application.class, args);
+	}
+
+}
---
+++ Practica2API/epo2/src/test/java/es/ubu/gii/sd/epo2/Epo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ubu.gii.sd.epo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Epo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
