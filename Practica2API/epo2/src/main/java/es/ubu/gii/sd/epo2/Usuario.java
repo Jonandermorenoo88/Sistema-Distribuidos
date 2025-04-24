c4b1ff815bef51d72d62f68d63aa93dad683932f
jonandermorenoo88 <jaincera97@gmail.com>
2025-04-24 09:57:41 +0200

    Subiendo Usuario.java

---
+++ Practica2API/epo2/src/main/java/es/ubu/gii/sd/epo2/Usuario.java
@@ -0,0 +1,22 @@
+package es.ubu.gii.sd.epo2;
+
+public class Usuario {
+    private Long id;
+    private String nombre;
+    private String usuario;
+    private String contrasena;
+
+    // Getters y setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getUsuario() { return usuario; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
+
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+}
